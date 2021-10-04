5f075d883f5eca859e3e585847bd2afb4103653e
Patrick Berzins <patrickberzins@gmail.com>
2021-10-03 23:06:45 -0400

    Finalization of Scatter

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
 	}
 	const incomeExt = d3.extent(incomeArray);
 	const lifeExt = d3.extent(lifeArray);
+	const popExt = d3.extent(popArray);
 	console.log('incomeExt', incomeExt);
 	console.log('lifeExt', lifeExt);
 
@@ -25,28 +26,213 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
 	let lifeMin = lifeExt[0]
 	let lifeMax = lifeExt[1]
 
-	const width = 700;
-	const height = 700;
-	d3.select('.chart').append('svg')
-	.attr('width', width)
-    .attr('height', height)
+	let popMin = popExt[0]
+	let popMax = popExt[1]
 
-	const yScale = d3
-    .scaleLinear()
-    .domain([lifeMin,lifeMax])
-    .range([0,700]) 
+	const margin = ({top: 20, right: 20, bottom: 40, left: 60})
+
+	const width = 850 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+	const svg = d3.select(".chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	const xScale = d3
     .scaleLinear()
     .domain([incomeMin,incomeMax])
-    .range([0,700]) 
+    .range([0,650]) 
+
+	const yScale = d3
+    .scaleLinear()
+    .domain([lifeMax,lifeMin])
+    .range([0,500]) 
+
+	const popScale = d3
+    .scaleLinear()
+    .domain([popMin,popMax])
+    .range([3,40]) 
+
+	const colorScale = d3.scaleOrdinal(d3.schemeTableau10)
+
 
 	console.log(xScale(incomeMax));
 	console.log(yScale(lifeMax));
 
 	console.log(data.Country)
 
-	d3.selectAll(".chart")
+	
+
+	svg.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('cx', d=>xScale(d.Income))
+  		.attr('cy', d=>yScale(d.LifeExpectancy))
+  		.attr('r', d=>popScale(d.Population))
+  		.attr('stroke', 'black')
+  		.attr('fill', d=>colorScale(d.Region))
+		.attr("fill-opacity","0.5")
+		.on("mouseover", function(event, d) {
+
+			const pos = d3.pointer(event, window)
+
+			var addComma = d3.format(",")
+	  
+			//Update the tooltip position and value
+			d3.select(".tooltip")
+			  .style("left", pos[0] + "px")
+			  .style("top", pos[1] + "px")
+			  .html(
+
+				"Country: " + d.Country +"<br>" + 
+				"Region: " + d.Region +"<br>" + 
+				"Population: " + addComma(d.Population) +"<br>" + 
+				"Income: " + addComma(d.Income) +"<br>" + 
+				"LifeExpectancy: " + d.LifeExpectancy +"<br>"
+
+			  );
+	  
+			//Show the tooltip
+			d3.select(".tooltip").classed("hidden", false);
+			
+
+		  })
+		  .on("mouseout", function(d) {
+			//Hide the tooltip
+			d3.select(".tooltip").classed("hidden", true);
+		  });
+
+	
+
+	const xAxis = d3.axisBottom()
+	.scale(xScale)
+	.ticks(10, "s")
+
+	svg.append("g")
+	.attr("class", "axis x-axis")
+	.call(xAxis)
+	.attr("transform", `translate(0, ${height - margin.bottom})`)
+
+	const yAxis = d3.axisLeft()
+	.scale(yScale)
+	.ticks(10, "s")
+
+	svg.append("g")
+	.attr("class", "axis y-axis")
+	.call(yAxis)
+	.attr("transform", `translate(0, 0)`)
+
+	svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", -50)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Life Expectancy");
+
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 120)
+    .attr("y", height)
+    .text("Income");
+
+
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 475)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#BCDBD8');
+
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 450)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#FAC79B');
+
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 425)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#A7BBD3');
+		  
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 400)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#F6E4A8')
+		.text("Hello");
+
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 375)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#F2ACAC');
+
+	svg.append('rect')
+		.attr('x', 500)
+  		.attr('y', 350)
+  		.attr('width', 15)
+  		.attr('height', 15)
+  		.attr('stroke', 'black')
+  		.attr('fill', '#ADD0A7');
+
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 180)
+    .attr("y", 363)
+    .text("America");
+
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 90)
+    .attr("y", 388)
+    .text("East Asia and Pacific");
+		
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 70)
+    .attr("y", 413)
+    .text("Europe and Central Asia");
+		
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 105)
+    .attr("y", 438)
+    .text("Sub-Saharan Africa");
+		
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 165)
+    .attr("y", 463)
+    .text("South Asia");
+		
+	svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 37)
+    .attr("y", 488)
+    .text("Middle East and North Africa");
+		
+		
 
 })
 
